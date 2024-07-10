5babe6dcc27990fc84dafc6e109bd5481bf77682
Kleanthis Liontis <kleanthisliontis@gmail.com>
2024-07-10 12:15:10 +0300

    working

--- script/DeployMerkleAirdrop.s.sol
+++ script/DeployMerkleAirdrop.s.sol
@@ -2,22 +2,24 @@
 
 pragma solidity ^0.8.26;
 
-import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
-import {RewardToken} from "../src/RewardToken.sol";
+import {MerkleAirdrop, IERC20} from "../src/MerkleAirdrop.sol";
 import {Script} from "forge-std/Script.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {RewardToken} from "../src/RewardToken.sol";
+import {console} from "forge-std/console.sol";
 
 contract DeployMerkleAirdrop is Script {
     bytes32 private s_merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
-    uint256 private s_amountToAirdropPerAddress = 4 * 25 * 1e18;
+    // 4 users, 25 tokens each
+    uint256 private AMOUNT_AIRDROPPED_PER_ADDRESS = 4 * 25 * 1e18;
 
     function deployMerkleAirdrop() public returns (MerkleAirdrop, RewardToken) {
         vm.startBroadcast();
-        RewardToken token = new RewardToken();
-        MerkleAirdrop airdrop = new MerkleAirdrop(s_merkleRoot, IERC20(address(token)));
-        token.mint(token.owner(), s_amountToAirdropPerAddress);
+        RewardToken rewardToken = new RewardToken();
+        MerkleAirdrop airdrop = new MerkleAirdrop(s_merkleRoot, IERC20(rewardToken));
+        rewardToken.mint(rewardToken.owner(), AMOUNT_AIRDROPPED_PER_ADDRESS);
+        IERC20(rewardToken).transfer(address(airdrop), AMOUNT_AIRDROPPED_PER_ADDRESS);
         vm.stopBroadcast();
-        return (airdrop, token);
+        return (airdrop, rewardToken);
     }
 
     function run() external returns (MerkleAirdrop, RewardToken) {
