be7ac569febff75dab17dcae207ef2fd58d6df89
Kleanthis Liontis <kleanthisliontis@gmail.com>
2024-07-11 11:57:11 +0300

    works on zksync

--- script/GenerateInput.s.sol
+++ script/GenerateInput.s.sol
@@ -17,6 +17,7 @@ contract GenerateInput is Script {
         types[0] = "address";
         types[1] = "uint";
         //addresses that can claim our tokens for testing purposes
+        //To change claiming addresses have to change here and rerun generate input and makemerkle scripts
         whitelist[0] = "0x6CA6d1e2D5347Bfab1d91e883F1915560e09129D";
         whitelist[1] = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
         whitelist[2] = "0x2ea3970Ed82D5b30be821FAAD4a731D35964F7dd";
